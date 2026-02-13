80356d391e9ade5aff0b9b00032778e89a957c8b
Calvin Lilley <calvin@solutus.co.za>
2026-02-13 11:40:16 +0200

    Add project files.

---
+++ SC.Mobile.POS.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SC.Mobile.POS.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
